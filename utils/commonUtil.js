daf0416aea126dba26f02c9e4eca28dc19ef020e
Gabriel <875886087@qq.com>
2017-12-05 17:13:27 +0800

    2017.12.05 17:13

--- utils/commonUtil.js
+++ utils/commonUtil.js
@@ -1,17 +1,1 @@
-define([],function(){
-	return {
-		render : temp => (){
-			document.body.innerHTML += temp;
-		},
-		getURLParameter : paramName => {
-		   	//构造一个含有目标参数的正则表达式的对象
- 			var reg = new RegExp("(^|&)" + paramName + "=([^&]*)(&|$)");
- 			//匹配目标参数
- 			var url = window.location.search.substr(1).match(reg);
- 			//返回参数值
- 			if(url != null)
- 				return unescape(url[2]);
- 			return null;
-		}
-	};
-}) 
+"use strict";define([],function(){return{render:function(n){document.body.innerHTML+=n},getURLParameter:function(n){var e=new RegExp("(^|&)"+n+"=([^&]*)(&|$)"),r=window.location.search.substr(1).match(e);return null!=r?unescape(r[2]):null}}});
