67e6cade492abad5997edd4f734c4002788c55b4
Gabriel <875886087@qq.com>
2017-12-07 19:27:15 +0800

    2017.12.7 19:26

--- utils/commonUtil.js
+++ utils/commonUtil.js
@@ -1,1 +1,1 @@
-"use strict";define([],function(){return{render:function(n){document.body.innerHTML+=n},getURLParameter:function(n){var e=new RegExp("(^|&)"+n+"=([^&]*)(&|$)"),r=window.location.search.substr(1).match(e);return null!=r?unescape(r[2]):null}}});
+"use strict";define([],function(){return{render:function(n){document.body.innerHTML+=n},getURLParameter:function(n){var e=new RegExp("(#|&)"+n+"=([^&]*)(&|$)"),r=window.location.href.match(e);return null!=r?unescape(r[2]):null}}});
