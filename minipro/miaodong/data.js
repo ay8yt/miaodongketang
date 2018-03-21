e4f09cac7cb359b171ec5d4e281c768c34081849
Gabriel <875886087@qq.com>
2018-03-21 11:46:46 +0800

    js

---
+++ minipro/miaodong/data.js
@@ -0,0 +1,269 @@
+var localdata = {
+    //算法系列
+    "algorithm": [
+        {
+            "name": "插入排序",
+            "videourl": "http://v.qq.com/x/page/p05308yhnyp.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484045&idx=1&sn=28f13c17b3c5eeedb338fb1a415f62c2&chksm=eaa8c579dddf4c6fce7763e7d96384343f459805411681a1d1afdbbd3d9cfbfc9cb7ca5bc418#rd",
+            "duration": "262",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/insert.jpeg"
+        },
+        {
+            "name": "希尔排序",
+            "videourl": "http://v.qq.com/x/page/u0530h9q3ko.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484064&idx=1&sn=620de60424e0634a5676141708b37d8b&chksm=eaa8c554dddf4c42e2ce2f0cfa2fe806b0eb97a250156318a5749da011dee1e10c3a8568f8f4#rd",
+            "duration": "386",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/shell.jpeg"
+        },
+        {
+            "name": "归并排序",
+            "videourl": "http://v.qq.com/x/page/q0530l6gcat.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484078&idx=1&sn=ff1cfafeb6a18bd094e67b5f48996e67&chksm=eaa8c55adddf4c4cb96ed0740bad5407d787e0e69b3b01e1501f78bf0e8274267747c4e6324a#rd",
+            "duration": "542",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/guibingpaixu.jpeg"
+        },
+        {
+            "name": "约瑟夫环",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "588",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/yuesefu.jpeg"
+        },
+        {
+            "name": "一致性哈希算法",
+            "videourl": "http://v.qq.com/x/page/c05301hihcc.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484023&idx=1&sn=8410d308a1bbcf86c5a4cb00dc5c217c&chksm=eaa8c583dddf4c95d5815600c1449d6d6a0806c56c6eace4bf64746d08856d25143a60810775#rd",
+            "duration": "599",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/yizhixinghash.jpeg"
+        },
+        {
+            "name": "BASE64",
+            "videourl": "http://v.qq.com/x/page/x05300g3o3j.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484031&idx=1&sn=7b6b91197a1a846b3925c9dac0be9ea9&chksm=eaa8c58bdddf4c9dfb5f657609bc7d59f778ada286957e1b2393c429f7af1753572b76537bb6#rd",
+            "duration": "208",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/base64.png"
+        },
+        {
+            "name": "什么是哈希",
+            "videourl": "https://v.qq.com/x/page/x0530xuhqcd.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484018&idx=1&sn=4757946cc052f07393219867b8c0e9e1&chksm=eaa8c586dddf4c90a35aad897105c4c05f03c0b21278a6e677de6a5eb475ab00f23bdee5cd6f#rd",
+            "duration": "473",
+            "active": '',
+            "thumbnail": "../../images/thumbnail/hash.png"
+        }
+    ],
+    //不懂装懂系列
+    "miaodong": [
+        {
+            "name": "面向对象01-起源",
+            "videourl": "http://v.qq.com/x/page/v0536wg0wh9.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484144&idx=1&sn=6d6c1467ce97d93bd24a6250947171f1&chksm=eaa8c504dddf4c124c57bfd2e002d2165f3575abbc3da751eaac66b1925f2cda0e97cbb4da72#rd",
+            "duration": "616",
+            "thumbnail": "../../images/thumbnail/OOstart.png"
+        },
+        {
+            "name": "面向对象02-发展",
+            "videourl": "http://v.qq.com/x/page/b0536hiqq1x.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484150&idx=1&sn=3740d030fbc4945a4d02d54dbae47df1&chksm=eaa8c502dddf4c1416d77f0ca7b4d8485baf29163a883182aeae750da15ce198e5608def2499#rd",
+            "duration": "341",
+            "thumbnail": "../../images/thumbnail/OOfazhan.png"
+        },
+        {
+            "name": "面向对象03-三大特征",
+            "videourl": "http://v.qq.com/x/page/r0536tan6n7.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484182&idx=1&sn=5d4c17470b5092db0cfd3f5d706a9a89&chksm=eaa8c4e2dddf4df4b2446ed802d4c4229d85830edc74db19bbc91911f6343b78d62e61504c74#rd",
+            "duration": "457",
+            "thumbnail": "../../images/thumbnail/OO3.png"
+        },
+        {
+            "name": "面向对象04-分析与设计",
+            "videourl": "http://v.qq.com/x/page/j0536nwv2ov.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484187&idx=1&sn=43dc2f5b436b9664b930b3d276807ef8&chksm=eaa8c4efdddf4df90e0785732f83fc24b7525c5ffc5f59b8a1b0c0d0ae4a7625d1530005489e#rd",
+            "duration": "502",
+            "thumbnail": "../../images/thumbnail/OOAOOD.png"
+        },
+        {
+            "name": "什么是CDN加速",
+            "videourl": "http://v.qq.com/x/page/n0538uzzhim.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484122&idx=1&sn=12cc79c2d1e3e3b16d8f6c692b533760&chksm=eaa8c52edddf4c384af7447ca16243a3cfc1ee59466ed7550cf2fd03887dddd6b555d112b777#rd",
+            "duration": "261",
+            "thumbnail": "../../images/thumbnail/CDN.png"
+        },
+        {
+            "name": "HTTPS到底安全在哪？",
+            "videourl": "http://v.qq.com/x/page/y0538o8z931.html",
+            "pageurl": "",
+            "duration": "467",
+            "thumbnail": "../../images/thumbnail/HTTPS.png"
+        },
+        {
+            "name": "比特币与区块链",
+            "videourl": "http://v.qq.com/x/page/d0533y0kegx.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484042&idx=1&sn=f7fb02e30ac8fe01a52a5c8af453300e&chksm=eaa8c57edddf4c685c4e8aa4fa370168efa0207915729bdba5c29bf657a6c2d2b433bdd150d2#rd",
+            "duration": "819",
+            "thumbnail": "../../images/thumbnail/bitcoin.png"
+        },
+        {
+            "name": "什么是引力波",
+            "videourl": "http://v.qq.com/x/page/n054664ubsw.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484232&idx=1&sn=036d0391b8651fcfabe10b1780c5efbc&chksm=eaa8c4bcdddf4daa0fb45d3c7c64e854082121b4d440320da1ea42c402d2286eb247ffd4f29e#rd",
+            "duration": "575",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/yinlibo.png"
+        }
+    ],
+    "design": [
+        {
+            "name": "单例模式",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "217",
+            "thumbnail": "../../images/thumbnail/singleton.png"
+        },
+        {
+            "name": "代理模式",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "315",
+            "thumbnail": "../../images/thumbnail/proxy.png"
+        },
+        {
+            "name": "策略模式",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "201",
+            "thumbnail": "../../images/thumbnail/strategy.png"
+        },
+        {
+            "name": "工厂模式",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "249",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/factory.png"
+        },
+        {
+            "name": "适配器模式",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "249",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/adapter.png"
+        },
+        {
+            "name": "责任链模式",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "232",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/chain.png"
+        }
+    ],
+    "language": [
+        {
+            "name": "COW语言",
+            "videourl": "",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484084&idx=1&sn=2d485cec0d07c0e7de18af1f83a172fa&chksm=eaa8c540dddf4c569aa649409550aa3a6baef791082b65b570b2692c5eb97bf590cb188b7687#rd",
+            "duration": "358",
+            "thumbnail": "../../images/thumbnail/COW.png"
+        }
+    ],
+    "math": [
+        {
+            "name": "美索不达米亚人的开平方法",
+            "videourl": "",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484086&idx=1&sn=659723658f221e36d28aec67e335ae1c&chksm=eaa8c542dddf4c540012f0453a95f5899158971dffe2b9d008894f48c3526e897c1d46432cd3#rd",
+            "duration": "358",
+            "thumbnail": "../../images/thumbnail/sqrt.png"
+        },
+        {
+            "name": "幸存者偏差",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "330",
+            "thumbnail": "../../images/thumbnail/xingcunzhepiancha.png"
+        }
+    ],
+    "movie": [
+        {
+            "name": "什么是蒙太奇？",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "1015",
+            "thumbnail": "../../images/thumbnail/Montage.png"
+        }
+    ],
+    "talkshow": [
+        {
+            "name": "知识的诅咒",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "215",
+            "thumbnail": "../../images/thumbnail/zhishidezuzhou.png"
+        },
+        {
+            "name": "信息的消化不良",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "402",
+            "thumbnail": "../../images/thumbnail/xinxixiaohuabuliang.png"
+        }
+    ],
+    "javascript": [
+        {
+            "name": "什么是函数式编程",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "238",
+            "thumbnail": "../../images/thumbnail/FUNC.png"
+        },
+        {
+            "name": "前端开发中的路由是指什么？",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "302",
+            "thumbnail": "../../images/thumbnail/router.png"
+        },
+        {
+            "name": "什么是AJAX",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "313",
+            "thumbnail": "../../images/thumbnail/ajax01.png"
+        },
+        {
+            "name": "什么是闭包",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "302",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/closure.png"
+        }
+    ],
+    "css": [
+        {
+            "name": "透视与3D效果",
+            "videourl": "http://v.qq.com/x/page/z0600z85o3a.html",
+            "pageurl": "https://mp.weixin.qq.com/s?__biz=MzI2NDYzOTEzMQ==&mid=2247484254&idx=1&sn=fb39616e8910d09faebc2e46854233c7&chksm=eaa8c4aadddf4dbcebe456d8ba63f48f84c4e0edc4a0e98a845d3db5bcdc028a84bce54e5775#rd",
+            "duration": "370",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/perspective.png"
+        },
+        {
+            "name": "弹性盒的放大与缩小",
+            "videourl": "",
+            "pageurl": "",
+            "duration": "313",
+            "new": "newest",
+            "thumbnail": "../../images/thumbnail/growshrink.png"
+        }
+    ]
+}
+
+export default localdata;
