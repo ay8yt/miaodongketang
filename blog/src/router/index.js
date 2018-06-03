7aad300afd3f009ab9776490da55d6ee4c498dbd
yintao <875886087@qq.com>
2018-06-03 20:21:48 +0800

    blog route -hash

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -9,7 +9,7 @@ const Article = () => import(/* webpackChunkName: "Article" */ '@/components/Art
 
 
 export default new Router({
-	mode: "history",
+	mode: "hash",
 	base: "/blog/dist/",  //以根目录为相对路径，这里的根目录是miaodongketang/，若作为单独项目部署时，则不需要配置base
   	routes: [
 	    {
